b63428c82fd74ee7fbfb92fe0089caea7404c9d1
Jonathan <jonacom@lissismore.com>
2016-07-11 09:41:45 -0600

    add before and after to the describle block

--- test/server-test.js
+++ test/server-test.js
@@ -2,19 +2,19 @@ const assert = require('assert');
 const request = require('request');
 const app = require('../server');
 
-before(done => {
-  this.port = 9876;
-  this.server = app.listen(this.port, (err, result) => {
-    if (err) { return done(err); }
-    done();
-  });
-});
-
-after(() => {
-  this.server.close();
-});
-
 describe('Server', () => {
+    before(done => {
+        this.port = 9876;
+        this.server = app.listen(this.port, (err, result) => {
+            if (err) { return done(err); }
+            done();
+        });
+    });
+
+    after(() => {
+        this.server.close();
+    });
+
     it('should exist', () => {
         assert(app);
     })
