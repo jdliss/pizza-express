f7004a8ea10059714026a50deb6c168805b2020e
Jonathan <jonacom@lissismore.com>
2016-07-11 10:43:10 -0600

    add pizzas route

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
 });
 
+app.post('/pizzas', (request, response) => {
+  response.sendStatus(201);
+});
+
 if (!module.parent) {
   app.listen(app.get('port'), () => {
     console.log(`${app.locals.title} is running on ${app.get('port')}.`);
