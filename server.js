6090fb4f9b00534f35996dfff93c5e05923ab18c
Jonathan <jonacom@lissismore.com>
2016-07-11 11:27:49 -0600

    done but the id's for the test don't match

--- server.js
+++ server.js
@@ -21,15 +21,19 @@ app.listen(app.get('port'), () => {
 });
 
 app.post('/pizzas', (request, response) => {
+  if (!request.body.pizza) { return response.sendStatus(400); }
+
   var id = generateId();
-  app.locals.pizzas[id] = request.body;
-  response.sendStatus(201);
+  app.locals.pizzas[id] = request.body.pizza;
+  response.redirect('/pizzas/' + id);
 });
+
 
 app.get('/pizzas/:id', (request, response) => {
   var pizza = app.locals.pizzas[request.params.id];
   response.render('pizza', { pizza: pizza });
 });
+
 
 if (!module.parent) {
   app.listen(app.get('port'), () => {
